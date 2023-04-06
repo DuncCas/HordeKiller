69925b243ce0480a6dba136a37b83fe9c36f6f77
DuncCas <lorenzo.castelli@fitstic-edu.com>
2023-04-06 15:35:46 +0200

    Created new Camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform followTarget;
+    public float followSpeed;
+
+    public float offset;
+    Vector3 cameraPos;
+
+    public Vector2 safeArea; // (X,Y) valori per i quali la telecamera non si muove
+
+    private void Awake() {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        followTarget.position += Vector3.up * offset;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Follow brutto
+        // transform.position = followTarget.position - offset;
+
+        // Follow morbido
+        cameraPos = Vector3.Lerp(transform.position, followTarget.position, Time.deltaTime * followSpeed);
+
+        if (Mathf.Abs(transform.position.x - followTarget.position.x) < safeArea.x)
+            cameraPos.x = transform.position.x;
+
+        transform.position = cameraPos;
+    }
+}
