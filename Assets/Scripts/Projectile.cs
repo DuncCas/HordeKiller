c1471a1cfc8169ede5c590f72acca18478fe3ac1
DuncCas <lorenzo.castelli@fitstic-edu.com>
2023-05-02 18:54:51 +0200

    Prepared code for handling bullet sounds

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public AudioClip[] shootSound;
+    public AudioClip[] shootLandingEnemy;
+    public AudioClip[] shootLandingWall;
 
     private int Lifetime = 4;
     public PlayerHandling pltmp;
@@ -17,7 +21,12 @@ public class Projectile : MonoBehaviour
     }
 
     private void OnEnable() {
+        //AudioSource.PlayClipAtPoint(pickShootSound(shootSound), transform.position, 0.5f);
         StartCoroutine(DestroyOverLifetime(Lifetime));
+    }
+
+    private AudioClip pickShootSound(AudioClip[] type) {
+        return type[(int)UnityEngine.Random.Range(0, type.Length)];
     }
 
     private void OnDisable() {
@@ -29,6 +38,11 @@ public class Projectile : MonoBehaviour
 
     public void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag != "Player") {
+            /*if (other.tag == "Enemy") {
+                AudioSource.PlayClipAtPoint(pickShootSound(shootLandingEnemy), transform.position, 0.5f);
+            } else {
+                AudioSource.PlayClipAtPoint(pickShootSound(shootLandingWall), transform.position, 0.5f);
+            }*/
             gameObject.SetActive(false);
         }
     }
